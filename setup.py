8d818b52784c58e5f7de68ec357378a82f6442b0
Tal Soikis <talsoikis@gmail.com>
2020-02-11 01:09:59 +0200

    deleted the parts about license

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gindex",
+    version="0.0.1",
+    author="Tal Soikis",
+    author_email="talsoikis@gmail.com",
+    description="A pure python geometrical indexing library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="", #TODO insert later
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3", #TODO change this thing and add license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
